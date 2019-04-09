8de10cb533f97d719d04b355d9a6bf4392df3512
Leonid <leonid.atf@gmail.com>
2019-04-09 14:43:01 +0300

    Added string extensions

---
+++ LS.Helpers.Hosting/Extensions/StringExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LS.Helpers.Hosting.Extensions
+{
+    /// <summary>
+    /// String extensions
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Converts to snakecase.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>string</returns>
+        public static string ToSnakeCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            var startUnderscores = Regex.Match(input, @"^_+");
+            return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        }
+
+        /// <summary>
+        /// Truncates the specified maximum length.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        /// <returns>string</returns>
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
+}

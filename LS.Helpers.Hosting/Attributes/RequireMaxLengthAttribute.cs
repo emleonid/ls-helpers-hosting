e20d3976183eb8ea4a4fca2fbc82f2422f6a64a6
Leonid <leonid.atf@gmail.com>
2019-04-23 18:46:11 +0300

    Added attributes

---
+++ LS.Helpers.Hosting/Attributes/RequireMaxLengthAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LS.Helpers.Hosting.Attributes
+{
+    /// <summary>
+    /// Max length attribute for model validation
+    /// </summary>
+    /// <seealso cref="ValidationAttribute" />
+    public class RequireMaxLengthAttribute : ValidationAttribute
+    {
+        private readonly int _maxlength;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequireMaxLengthAttribute"/> class.
+        /// </summary>
+        /// <param name="maxlength">The maxlength.</param>
+        public RequireMaxLengthAttribute(int maxlength)
+        {
+            _maxlength = maxlength;
+        }
+
+        /// <inheritdoc />
+        protected override ValidationResult IsValid(
+            object value,
+            ValidationContext validationContext)
+        {
+            if (value is string str)
+            {
+                if (str.Length > _maxlength)
+                {
+                    var errorMessage = $"{validationContext.MemberName} length can't be more than {_maxlength}.";
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            else
+            {
+                return new ValidationResult($"{validationContext.MemberName} should be string type");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ LS.Helpers.Hosting/Attributes/ValidateModelStateAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using LS.Helpers.Hosting.API;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LS.Helpers.Hosting.Attributes
+{
+    /// <summary>
+    /// ValidateModelStateAttribute
+    /// </summary>
+    /// <seealso cref="ActionFilterAttribute" />
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="context"></param>
+        /// <inheritdoc />
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var allErrors = context.ModelState
+                    .Values
+                    .SelectMany(v => v.Errors)
+                    .Select(x=>new ErrorInfo(x.ErrorMessage))
+                    .ToList();
+
+                context.Result = new BadRequestObjectResult(
+                    new ExecutionResult<object>(allErrors)); 
+            }
+        }
+    }
+}

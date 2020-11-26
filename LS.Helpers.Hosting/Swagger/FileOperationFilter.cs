a3b1e45b70f8d1848fd118b1fd0cbbedbeacd341
Leonid <leonid.atf@gmail.com>
2020-11-26 13:08:25 +0200

    Update

--- LS.Helpers.Hosting/Swagger/FileOperationFilter.cs
+++ LS.Helpers.Hosting/Swagger/FileOperationFilter.cs
@@ -1,44 +1,44 @@
-﻿namespace LS.Helpers.Hosting.Swagger
-{
-    using System.Linq;
-    using JetBrains.Annotations;
-    using Microsoft.AspNetCore.Http;
-    using Swashbuckle.AspNetCore.Swagger;
-    using Swashbuckle.AspNetCore.SwaggerGen;
+﻿//namespace LS.Helpers.Hosting.Swagger
+//{
+//    using System.Linq;
+//    using JetBrains.Annotations;
+//    using Microsoft.AspNetCore.Http;
+//    using Swashbuckle.AspNetCore.Swagger;
+//    using Swashbuckle.AspNetCore.SwaggerGen;
 
-    /// <summary>
-    /// FileOperationFilter
-    /// </summary>
-    /// <seealso cref="IOperationFilter" />
-    [UsedImplicitly]
-    public class FileOperationFilter : IOperationFilter
-    {
-        /// <summary>
-        /// Applies the specified operation.
-        /// </summary>
-        /// <param name="operation">The operation.</param>
-        /// <param name="context">The context.</param>
-        public void Apply(Operation operation, OperationFilterContext context)
-        {
-            if (context.ApiDescription.ParameterDescriptions.Any(
-                x => x.ModelMetadata.ContainerType == typeof(IFormFile)))
-            {
-                var list = operation.Parameters.Where(x => !x.Name.StartsWith("File.")).ToList();
-                operation.Parameters.Clear();
-                foreach (var parameter in list)
-                {
-                    operation.Parameters.Add(parameter);
-                }
-                operation.Parameters.Add(new NonBodyParameter
-                {
-                    Name = "File",
-                    In = "formData",
-                    Description = "Upload file.",
-                    Required = true,
-                    Type = "file"
-                });
-                operation.Consumes.Add("multipart/form-data");
-            }
-        }
-    }
-}
+//    /// <summary>
+//    /// FileOperationFilter
+//    /// </summary>
+//    /// <seealso cref="IOperationFilter" />
+//    [UsedImplicitly]
+//    public class FileOperationFilter : IOperationFilter
+//    {
+//        /// <summary>
+//        /// Applies the specified operation.
+//        /// </summary>
+//        /// <param name="operation">The operation.</param>
+//        /// <param name="context">The context.</param>
+//        public void Apply(Operation operation, OperationFilterContext context)
+//        {
+//            if (context.ApiDescription.ParameterDescriptions.Any(
+//                x => x.ModelMetadata.ContainerType == typeof(IFormFile)))
+//            {
+//                var list = operation.Parameters.Where(x => !x.Name.StartsWith("File.")).ToList();
+//                operation.Parameters.Clear();
+//                foreach (var parameter in list)
+//                {
+//                    operation.Parameters.Add(parameter);
+//                }
+//                operation.Parameters.Add(new NonBodyParameter
+//                {
+//                    Name = "File",
+//                    In = "formData",
+//                    Description = "Upload file.",
+//                    Required = true,
+//                    Type = "file"
+//                });
+//                operation.Consumes.Add("multipart/form-data");
+//            }
+//        }
+//    }
+//}
--- LS.Helpers.Hosting/Swagger/FormFileOperationFilter.cs
+++ LS.Helpers.Hosting/Swagger/FormFileOperationFilter.cs
@@ -1,67 +1,67 @@
-﻿namespace LS.Helpers.Hosting.Swagger
-{
-    using System.Collections.Generic;
-    using System.Linq;
-    using JetBrains.Annotations;
-    using Microsoft.AspNetCore.Mvc.ApiExplorer;
-    using Swashbuckle.AspNetCore.Swagger;
-    using Swashbuckle.AspNetCore.SwaggerGen;
+﻿//namespace LS.Helpers.Hosting.Swagger
+//{
+//    using System.Collections.Generic;
+//    using System.Linq;
+//    using JetBrains.Annotations;
+//    using Microsoft.AspNetCore.Mvc.ApiExplorer;
+//    using Swashbuckle.AspNetCore.Swagger;
+//    using Swashbuckle.AspNetCore.SwaggerGen;
 
-    /// <summary>
-    /// FormFileOperationFilter
-    /// </summary>
-    /// <seealso cref="IOperationFilter" />
-    [UsedImplicitly]
-    public class FormFileOperationFilter : IOperationFilter
-    {
-        /// <summary>
-        /// Applies the specified operation.
-        /// </summary>
-        /// <param name="operation">The operation.</param>
-        /// <param name="schemaRegistry">The schema registry.</param>
-        /// <param name="apiDescription">The API description.</param>
-        public void Apply(Operation operation, ISchemaRegistry schemaRegistry, ApiDescription apiDescription)
-        {
-            var requestAttributes =
-                apiDescription.ActionAttributes().Where(x => x is SwaggerFormAttribute);
+//    /// <summary>
+//    /// FormFileOperationFilter
+//    /// </summary>
+//    /// <seealso cref="IOperationFilter" />
+//    [UsedImplicitly]
+//    public class FormFileOperationFilter : IOperationFilter
+//    {
+//        /// <summary>
+//        /// Applies the specified operation.
+//        /// </summary>
+//        /// <param name="operation">The operation.</param>
+//        /// <param name="schemaRegistry">The schema registry.</param>
+//        /// <param name="apiDescription">The API description.</param>
+//        public void Apply(Operation operation, ISchemaRegistry schemaRegistry, ApiDescription apiDescription)
+//        {
+//            var requestAttributes =
+//                apiDescription.ActionAttributes().Where(x => x is SwaggerFormAttribute);
 
-            foreach (SwaggerFormAttribute attr in requestAttributes)
-            {
-                var list = new List<IParameter>
-                {
-                    new NonBodyParameter
-                    {
-                        Name = "File",
-                        In = "formData",
-                        Description = "Upload file.",
-                        Required = true,
-                        Type = "file"
-                    }
-                };
-                if (operation.Parameters != null)
-                {
-                    foreach (var parameter in list)
-                    {
-                        operation.Parameters.Add(parameter);
-                    }
-                }
-                else
-                {
-                    operation.Parameters = list;
-                }
+//            foreach (SwaggerFormAttribute attr in requestAttributes)
+//            {
+//                var list = new List<IParameter>
+//                {
+//                    new NonBodyParameter
+//                    {
+//                        Name = "File",
+//                        In = "formData",
+//                        Description = "Upload file.",
+//                        Required = true,
+//                        Type = "file"
+//                    }
+//                };
+//                if (operation.Parameters != null)
+//                {
+//                    foreach (var parameter in list)
+//                    {
+//                        operation.Parameters.Add(parameter);
+//                    }
+//                }
+//                else
+//                {
+//                    operation.Parameters = list;
+//                }
 
-                operation.Consumes.Add("multipart/form-data");
-            }
-        }
+//                operation.Consumes.Add("multipart/form-data");
+//            }
+//        }
 
-        /// <summary>
-        /// Applies the specified operation.
-        /// </summary>
-        /// <param name="operation">The operation.</param>
-        /// <param name="context">The context.</param>
-        public void Apply(Operation operation, OperationFilterContext context)
-        {
-            Apply(operation, context.SchemaRegistry, context.ApiDescription);
-        }
-    }
-}
+//        /// <summary>
+//        /// Applies the specified operation.
+//        /// </summary>
+//        /// <param name="operation">The operation.</param>
+//        /// <param name="context">The context.</param>
+//        public void Apply(Operation operation, OperationFilterContext context)
+//        {
+//            Apply(operation, context.SchemaRegistry, context.ApiDescription);
+//        }
+//    }
+//}

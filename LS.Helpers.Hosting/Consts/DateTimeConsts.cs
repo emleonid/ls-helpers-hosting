7db7cd6fad089ea5df21322c5240ab23b0a769bd
Leonid <leonid.atf@gmail.com>
2021-08-18 22:27:43 +0300

    Added NodaTime

---
+++ LS.Helpers.Hosting/Consts/DateTimeConsts.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="DateTimeConsts.cs" company="Legiosoft">
+// © Legiosoft
+// </copyright>
+
+namespace LS.Helpers.Hosting.Consts
+{
+    /// <summary>
+    /// DateTimeConsts
+    /// </summary>
+    public static class DateTimeConsts
+    {
+        /// <summary>
+        /// TimeZone
+        /// </summary>
+        public static class TimeZone
+        {
+            /// <summary>
+            /// The Kyiv timezone in TZ DB
+            /// </summary>
+            public const string Kyiv = "Europe/Kiev";
+        }
+    }
+}
---
+++ LS.Helpers.Hosting/Extensions/NodaTime/LocalDateExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace LS.Helpers.Hosting.Extensions.NodaTime
+{
+    using Consts;
+    using global::NodaTime;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// NodaTime LocalDate Extensions
+    /// </summary>
+    [UsedImplicitly]
+    public static class LocalDateExtensions
+    {
+        /// <summary>
+        /// Gets the kyiv today local date now.
+        /// </summary>
+        /// <param name="date">The date.</param>
+        /// <returns></returns>
+        public static LocalDate GetKyivToday(this LocalDate date)
+        {
+            var instant = SystemClock.Instance.GetCurrentInstant();
+            var dateTimeZone = DateTimeZoneProviders.Tzdb[DateTimeConsts.TimeZone.Kyiv];
+            var localDate = instant.InZone(dateTimeZone).Date;
+
+            return localDate;
+        }
+    }
+}
